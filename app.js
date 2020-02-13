1a2311ca366eb17937611cb6f1ab7692592fe38a
allicsander <me@evergoal.ru>
2020-02-13 16:39:06 +0300

    added key adn click event listeners

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ var uIController = (function() {
 })();
 
 
-var appController = (function() {
+var appController = (function(budgetCtrl, uICtrl) {
+
+    var controlAddItem = function(){
+        console.log('controlAddItem function is called'); 
+    }
     
-})();
+    document.querySelector('.add__btn').addEventListener('click', controlAddItem);
+
+    document.addEventListener('keypress', function(e){
+         if(e.keyCode === 13 || e.which === 13){
+            controlAddItem();
+         }
+    });
+
+})((budgetController, uIController));
 
 
